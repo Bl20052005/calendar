6755df8e576b8ce86b737dc5e58baca6b762533f
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2023-06-12 18:54:36 -0700

    there are too many lines of code~

--- src/calendar_menu.js
+++ src/calendar_menu.js
@@ -290,11 +290,26 @@ function AddEventPopUpTime({previousTime = {hour: 0, minute: 0}, setPreviousTime
     )
 }
 
-function ChooseColorCheck() {
-    
+function ChooseColorPalate({selectedColor, setSelectedColor}) {
+    let colors = ["#9fc0f5", "#4332d9", "#ae99e0", "#320699", "#c979bf", "#8a0e79", "#cf5f66", "#9e0812", "#93db7f", "#26820d", "#7adedc", "#0da3a1"];
+
+    let returnArr = colors.map((color, index) => {
+        return(
+            <div className='color-palate' key={'color-palate-container-' + index}>
+                <div style={{"backgroundColor" : color, "border" : color === selectedColor ? "3px solid white" : ""}} className='color-palate-color' onClick={() => setSelectedColor(color)}></div>
+                {/* <input type="text" placeholder='Enter label here...' className='color-palate-label'></input> */}
+            </div>
+        )
+    });
+
+    return(
+        <div className='color-palate-container'>
+            {returnArr}
+        </div>
+    )
 }
 
-function AddEventPopUp() {
+function AddEventPopUp({isThisVisible, setIsThisVisible, dispatch}) {
 
     const getTimeIn15MinuteIntervals = (hour, minute, wantedFunction) => {
         let returnStr = "";
@@ -346,6 +361,33 @@ function AddEventPopUp() {
     const [previousTime, setPreviousTime] = useState({hour: currentDateOne.getHours() + Math.floor((Math.ceil(currentDateOne.getMinutes() / 15) * 15) / 60), minute: (Math.ceil(currentDateOne.getMinutes() / 15) * 15) % 60});
     const [curTimeOne, setCurTimeOne] = useState(getTimeIn15MinuteIntervals(currentDateOne.getHours(), currentDateOne.getMinutes()));
     const [curTimeTwo, setCurTimeTwo] = useState(getTimeIn15MinuteIntervals(currentDateTwo.getHours(), currentDateTwo.getMinutes()));
+    const [isMouseDown, setIsMouseDown] = useState(false);
+    const [originalCoords, setOriginalCoords] = useState([0,0]);
+    const [selectedColor, setSelectedColor] = useState("#9fc0f5");
+    const [curTitle, setCurTitle] = useState("");
+    const [curDescription, setCurDescription] = useState("");
+    const [wrongInputs, setWrongInputs] = useState({"time1" : "", "time2" : "", "date1" : "", "date2": ""});
+
+    const resetAll = () => {
+        setCurDateOne({"year": currentDateOne.getFullYear(), "month": currentDateOne.getMonth(), "day": currentDateOne.getDate()});
+        setCurDateTwo({"year": currentDateTwo.getFullYear(), "month": currentDateTwo.getMonth(), "day": currentDateTwo.getDate()});
+        setDateOneInput(convertMonths[currentDateOne.getMonth()] + " " + currentDateOne.getDate() + ", " + currentDateOne.getFullYear());
+        setDateTwoInput(convertMonths[currentDateTwo.getMonth()] + " " + currentDateTwo.getDate() + ", " + currentDateTwo.getFullYear());
+        setCurLocation("");
+        setFocusCalendarVisibleOne("visibility-hidden");
+        setFocusCalendarVisibleTwo("visibility-hidden");
+        setFocusTimeVisibleOne("visibility-hidden");
+        setFocusTimeVisibleTwo("visibility-hidden");
+        setPreviousTime({hour: currentDateOne.getHours() + Math.floor((Math.ceil(currentDateOne.getMinutes() / 15) * 15) / 60), minute: (Math.ceil(currentDateOne.getMinutes() / 15) * 15) % 60});
+        setCurTimeOne(getTimeIn15MinuteIntervals(currentDateOne.getHours(), currentDateOne.getMinutes()));
+        setCurTimeTwo(getTimeIn15MinuteIntervals(currentDateTwo.getHours(), currentDateTwo.getMinutes()));
+        setIsMouseDown(false);
+        setOriginalCoords([0,0]);
+        setSelectedColor("#9fc0f5");
+        setCurTitle("");
+        setCurDescription("");
+        setWrongInputs({"time1" : "", "time2" : "", "date1" : "", "date2": ""});
+    }
 
     const ref1 = useRef();
 
@@ -433,14 +475,100 @@ function AddEventPopUp() {
                 setCurTimeTwo(getTimeIn15MinuteIntervals(dateNow.getHours(), dateNow.getMinutes()));
             }
         }
-    }, [curTimeOne, dateOneInput, dateTwoInput])
+    }, [curTimeOne, dateOneInput, dateTwoInput]);
+
+    useEffect(() => {
+        let finalObj = {}
+        if(isNaN(Date.parse("01 01 01 " + curTimeOne)) || curTimeOne === "") finalObj["time1"] = "input-incorrect";
+        else finalObj["time1"] = "";
+        if(isNaN(Date.parse("01 01 01 " + curTimeTwo)) || curTimeTwo === "") finalObj["time2"] = "input-incorrect";
+        else finalObj["time2"] = "";
+        if(isNaN(Date.parse(dateOneInput)) || dateOneInput.length < 5) finalObj["date1"] = "input-incorrect";
+        else finalObj["date1"] = "";
+        if(isNaN(Date.parse(dateTwoInput)) || dateTwoInput.length < 5) finalObj["date2"] = "input-incorrect";
+        else finalObj["date2"] = "";
+
+        if(finalObj.time1 !== "input-incorrect" && finalObj.time2 !== "input-incorrect" && finalObj.date1 !== "input-incorrect" && finalObj.date2 !== "input-incorrect") {
+            let dayOne = new Date(dateOneInput)
+            let dayTwo = new Date(dateTwoInput)
+            dayOne = new Date(dayOne.getMonth() + 1 + " " + dayOne.getDate() + " " + dayOne.getFullYear() + " " + curTimeOne);
+            dayTwo = new Date(dayTwo.getMonth() + 1 + " " + dayTwo.getDate() + " " + dayTwo.getFullYear() + " " + curTimeTwo);
+            if(dayOne.getTime() > dayTwo.getTime()) {
+                if(dayOne.getFullYear() === dayTwo.getFullYear() && dayOne.getMonth() === dayTwo.getMonth() && dayOne.getDate() === dayTwo.getDate()) {
+                    finalObj["time1"] = "input-incorrect";
+                } else {
+                    finalObj["time1"] = "input-incorrect";
+                    finalObj["date1"] = "input-incorrect";
+                }
+            }
+        }
+
+        setWrongInputs(finalObj);
+
+    }, [curTimeOne, curTimeTwo, dateOneInput, dateTwoInput]);
+
+    const handleMoveMouseDown = (e) => {
+        setIsMouseDown(true);
+        let top = e.target.getBoundingClientRect().top;
+        let left = e.target.getBoundingClientRect().left;
+        setOriginalCoords([e.clientY - top, e.clientX - left]);
+    }
+
+    const handleMoveMouseMove = (e) => {
+        if(isMouseDown) {
+            let effectedClass = document.querySelector('.add-event-popup');
+            effectedClass.style.top = (e.clientY - originalCoords[0]) + "px";
+            effectedClass.style.left = (e.clientX - originalCoords[1]) + "px";
+        }
+    }
+
+    document.onmousemove = handleMoveMouseMove;
+
+    const handleMoveMouseUp = () => {
+        setIsMouseDown(false);
+        document.onmousemove = null;
+    }
+
+    const handleClickEventExit = () => {
+        setIsThisVisible("visibility-hidden")
+        let effectedClass = document.querySelector('.add-event-popup');
+        effectedClass.style.top = "calc((100% - 585px) / 2)";
+        effectedClass.style.left = "calc((100% - 430px) / 2)";
+        resetAll();
+    }
+
+    const handleClickEventSave = () => {
+        if(wrongInputs.time1 !== "input-incorrect" && wrongInputs.time2 !== "input-incorrect" && wrongInputs.date1 !== "input-incorrect" && wrongInputs.date2 !== "input-incorrect") {
+            let finalObj = {};
+            if(curTitle === "") {
+                finalObj["title"] = "[No Title]";
+            } else {
+                finalObj["title"] = curTitle;
+            }
+            let dayOne = new Date(dateOneInput);
+            let dayTwo = new Date(dateTwoInput);
+            finalObj["startTime"] = dayOne.getMonth() + 1 + " " + dayOne.getDate() + " " + dayOne.getFullYear() + " " + curTimeOne;
+            finalObj["endTime"] = dayTwo.getMonth() + 1 + " " + dayTwo.getDate() + " " + dayTwo.getFullYear() + " " + curTimeTwo;
+            finalObj["rawStartDate"] = dateOneInput;
+            finalObj["rawStartTime"] = curTimeOne;
+            finalObj["rawEndDate"] = dateTwoInput;
+            finalObj["rawEndTime"] = curTimeTwo;
+            finalObj["color"] = selectedColor;
+            finalObj["location"] = curLocation;
+            finalObj["description"] = curDescription;
+
+            dispatch(addEvent(finalObj));
+
+            handleClickEventExit();
+        }
+    }
 
     return(
-        <div className='add-event-popup'>
-            <div className='add-event-popup-move'>
-                <svg className='add-event-popup-exit' xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+        <div className={'add-event-popup add-event-popup-position ' + isThisVisible}>
+            <div className='add-event-popup-move' onMouseDown={(e) => handleMoveMouseDown(e)} onMouseUp={(e) => handleMoveMouseUp(e)}>
+                <svg className='add-event-popup-exit' onClick={() => handleClickEventExit()} xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
             </div>
-            <input type="text" className='add-event-popup-title' name="add-event-popup-title" placeholder='Enter title here'></input>
+            <input type="text" className='add-event-popup-title' name="add-event-popup-title" placeholder='Enter title here' value={curTitle} onChange={(e) => setCurTitle(e.target.value)}></input>
             <div className='add-event-popup-time-container'>
             <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
                 <div className='add-event-popup-texts'>Time</div>
@@ -448,24 +576,24 @@ function AddEventPopUp() {
             <div className='add-event-popup-time'>
                 <div className='add-event-popup-time-sub'>
                     <div className='add-event-popup-time-sub1-ref' ref={ref1}>
-                        <input type="text" className='add-event-popup-time-date' placeholder='Enter date here' name="add-event-popup-time-date-1" value={dateOneInput} onChange={handleSetCurDateOne} onFocus={() => setFocusCalendarVisibleOne("visibility-visible")}></input>
+                        <input type="text" className={'add-event-popup-time-date ' + wrongInputs.date1} placeholder='Enter date here' name="add-event-popup-time-date-1" value={dateOneInput} onChange={handleSetCurDateOne} onFocus={() => setFocusCalendarVisibleOne("visibility-visible")}></input>
                         <AddEventPopupCalendar currentDate={curDateOne} setCurrentDate={setCurDateOne} setDateInput={setDateOneInput} focusCalendarVisible={focusCalendarVisibleOne}/>
                     </div>
                     <div>at</div>
                     <div className='add-event-popup-time-sub3-ref' ref={ref3}>
-                        <input type="text" className='add-event-popup-time-time' name="add-event-popup-time-time" value={curTimeOne} onChange={(e) => setCurTimeOne(e.target.value)} onFocus={() => setFocusTimeVisibleOne("visibility-visible")}></input>
+                        <input type="text" className={'add-event-popup-time-time ' + wrongInputs.time1} name="add-event-popup-time-time" value={curTimeOne} onChange={(e) => setCurTimeOne(e.target.value)} onFocus={() => setFocusTimeVisibleOne("visibility-visible")}></input>
                         <AddEventPopUpTime previousTime={{hour: 0, minute: 0}} setPreviousTime={setPreviousTime} setCurTime={setCurTimeOne} isVisible={focusTimeVisibleOne} curTime={curTimeOne}/>
                     </div>
                 </div>
                 <div className='add-event-popup-time-to'>to</div>
                 <div className='add-event-popup-time-sub'>
                 <div className='add-event-popup-time-sub2-ref' ref={ref2}>
-                    <input type="text" className='add-event-popup-time-date' placeholder='Enter date here' name="add-event-popup-time-date-1" value={dateTwoInput} onChange={handleSetCurDateTwo} onFocus={() => setFocusCalendarVisibleTwo("visibility-visible")}></input>
+                    <input type="text" className={'add-event-popup-time-date ' + wrongInputs.date2} placeholder='Enter date here' name="add-event-popup-time-date-1" value={dateTwoInput} onChange={handleSetCurDateTwo} onFocus={() => setFocusCalendarVisibleTwo("visibility-visible")}></input>
                     <AddEventPopupCalendar currentDate={curDateTwo} setCurrentDate={setCurDateTwo} setDateInput={setDateTwoInput} focusCalendarVisible={focusCalendarVisibleTwo}/>
                 </div>
                     <div>at</div>
                     <div className='add-event-popup-time-sub4-ref' ref={ref4}>
-                        <input type="text" className='add-event-popup-time-time' name="add-event-popup-time-time" value={curTimeTwo} onChange={(e) => setCurTimeTwo(e.target.value)} onFocus={() => setFocusTimeVisibleTwo("visibility-visible")}></input>
+                        <input type="text" className={'add-event-popup-time-time ' + wrongInputs.time2} name="add-event-popup-time-time" value={curTimeTwo} onChange={(e) => setCurTimeTwo(e.target.value)} onFocus={() => setFocusTimeVisibleTwo("visibility-visible")}></input>
                         <AddEventPopUpTime previousTime={curDateOne.year === curDateTwo.year && curDateOne.month === curDateTwo.month && curDateOne.day === curDateTwo.day ? previousTime : {hour: 0, minute: 0}} setCurTime={setCurTimeTwo} isVisible={focusTimeVisibleTwo} curTime={curTimeTwo}/>
                     </div>
                 </div>
@@ -479,25 +607,29 @@ function AddEventPopUp() {
                 </div>
                 <div className='add-event-popup-texts'>Description</div>
             </div>
-            <textarea className='add-event-popup-description'></textarea>
+            <textarea className='add-event-popup-description' name='add-event-popup-description' value={curDescription} onChange={(e) => setCurDescription(e.target.value)}></textarea>
             <div className='add-event-popup-color-container'>
                 <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 576 512"><path d="M339.3 367.1c27.3-3.9 51.9-19.4 67.2-42.9L568.2 74.1c12.6-19.5 9.4-45.3-7.6-61.2S517.7-4.4 499.1 9.6L262.4 187.2c-24 18-38.2 46.1-38.4 76.1L339.3 367.1zm-19.6 25.4l-116-104.4C143.9 290.3 96 339.6 96 400c0 3.9 .2 7.8 .6 11.6C98.4 429.1 86.4 448 68.8 448H64c-17.7 0-32 14.3-32 32s14.3 32 32 32H208c61.9 0 112-50.1 112-112c0-2.5-.1-5-.2-7.5z"/></svg>
                 <div className='add-event-popup-texts'>Color</div>
             </div>
+            <ChooseColorPalate selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
             <div className='add-event-popup-location-container'>
                 <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
                 <div className='add-event-popup-texts'>Location</div>
             </div>
             <input type="text" className='add-event-popup-location' name="add-event-popup-location" value={curLocation} onChange={(e) => setCurLocation(e.target.value)}></input>
-            <button>Submit</button>
-            <button>Cancel</button>
+            <div className='add-event-popup-save'>
+                <div className='add-event-popup-button' onClick={() => handleClickEventSave()}>Save</div>
+                <div className='add-event-popup-button' onClick={() => handleClickEventExit()}>Cancel</div>
+            </div>
+            
         </div>
     );
 }
 
-function AddEvent() {
+function AddEvent({setAddEventPopUpVisible}) {
     return(
-        <div className='add-event-container'>
+        <div className='add-event-container' onClick={() => setAddEventPopUpVisible("visibility-visible")}>
             <div className='add-event-add-sign'>
                 <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
             </div>
@@ -511,8 +643,9 @@ function TodayEvents({currentEvents}) {
     const currentEventsJSX = currentEvents.map((item, index) =>
         <div className='today-events-item-container' key={"today-events-" + index}>
             <div className='today-events-item'>
-                <div className="today-events-item-color" style={{"backgroundColor": item[1]}}></div>
-                <div className='today-events-item-name'>{item[0]}</div>
+                <div className="today-events-item-color" style={{"backgroundColor": item.color}}></div>
+                <div className="today-events-item-time">{item.rawStartTime}</div>
+                <div className='today-events-item-name'>{item.title}</div>
             </div>
             <div className='today-events-divider-line'></div>
         </div>
@@ -528,14 +661,19 @@ function TodayEvents({currentEvents}) {
     
 }
 
+function ColorChooser() {
+    
+}
+
 function Menu() {
     const currentEvents = useSelector((state) => state.event.events);
     const currentDate = useSelector((state) => state.date);
     const dispatch = useDispatch();
+    const [addEventPopUpVisible, setAddEventPopUpVisible] = useState("visibility-hidden");
     return(
         <div className="calendar-menu">
-            <AddEvent />
-            <AddEventPopUp />
+            <AddEvent setAddEventPopUpVisible={setAddEventPopUpVisible}/>
+            <AddEventPopUp isThisVisible={addEventPopUpVisible} setIsThisVisible={setAddEventPopUpVisible} dispatch={dispatch}/>
             <div className='calendar-menu-divider-line'></div>
             <TodayEvents currentEvents={currentEvents}/>
             <div className='calendar-menu-divider-line'></div>
